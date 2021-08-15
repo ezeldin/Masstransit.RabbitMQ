9f40a365a4f50a1823d4ed9cd6100da90211f765
ezeldin <ezeldin_hussein@hotmail.com>
2021-08-15 22:37:32 +0300

    add Ticketing Microservice and its consumer

---
+++ Ticketing.Consumer/Consumers/TicketConsumer.cs
@@ -0,0 +1,20 @@
+﻿using DemandManagment.MessagesContract;
+using MassTransit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ticketing.Consumer.Consumers
+{
+    public class TicketConsumer : IConsumer<Ticket>
+    {
+        public async Task Consume(ConsumeContext<Ticket> context)
+        {
+            var data = context.Message;
+            //Validate the Ticket Data
+            //Store to Database
+            //Notify the user via Email / SMS
+        }
+    }
+}
